318dae908df85cdf718aee1d31eabd36ffe39322
Matías Daza <matias.daza@uc.cl>
2023-02-28 14:22:19 -0300

    First commit

---
+++ src/main/java/cl/com/aquavolley/AquaVolleyApplication.java
@@ -0,0 +1,13 @@
+package cl.com.aquavolley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaVolleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquaVolleyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/com/aquavolley/AquaVolleyApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.com.aquavolley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquaVolleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
